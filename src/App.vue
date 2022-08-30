572ad386b1de891fb59702546e96b1186882eeb3
ethanimooney <ethanimooney@gmail.com>
2022-08-29 19:28:15 -0500

    Removes pregenerated Vue content and sets up for project development.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
+    <div class="pageContentContainer">
+      <SearchBar />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from "./components/SearchBar.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SearchBar,
+    NavBar,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sofia-pro, sans-serif;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="searchBarContainer">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
