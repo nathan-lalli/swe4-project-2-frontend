117f667da762f952e0e8d78448ddef58f7626d15
Logan Demaray <l.demaray@eagles.oc.edu>
2022-09-12 12:58:43 -0500

    Added Pagination.vue and App.vue has methods to set the pages it is on.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
       <div class="courseListTemp"></div>
     </div>
     <PopUpModal v-show="isPopupVisible" @close="closePopup" />
+    <pagination
+      :totalPages="10"
+      :perPage="10"
+      :currentPage="currentPage"
+      @pagechanged="onPageChange"
+    />
   </div>
 </template>
 
@@ -13,6 +19,7 @@
 import SearchBar from "./components/SearchBar.vue";
 import NavBar from "./components/NavBar.vue";
 import PopUpModal from "./components/PopUpModal.vue";
+import Pagination from "./components/Pagination.vue";
 
 export default {
   name: "App",
@@ -20,10 +27,12 @@ export default {
     SearchBar,
     NavBar,
     PopUpModal,
+    Pagination,
   },
   data() {
     return {
       isPopupVisible: false,
+      currentPage: 1,
     };
   },
   methods: {
@@ -32,6 +41,10 @@ export default {
     },
     closePopup() {
       this.isPopupVisible = false;
+    },
+    onPageChange(page) {
+      console.log(page);
+      this.currentPage = page;
     },
   },
 };
