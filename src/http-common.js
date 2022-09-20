f9125736e3fd868c7b56d2e7ba455b05f3115277
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-09-20 13:56:32 -0500

    Fix routing

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3003",
+  baseURL: "project2.eaglesoftwareteam.com",
   headers: {
     "Content-type": "application/json",
   },
